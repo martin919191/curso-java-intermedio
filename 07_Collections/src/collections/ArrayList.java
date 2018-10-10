71ee5e6976cfb6bf75c92f0822456f67e90bad8a
Martín Isusi Seff <martinisusi@hotmail.com>
2018-10-09 21:46:09 -0300

    Agrego la implementación del método add y agrego el genérico en la interfaz List

--- 07_Collections/src/collections/ArrayList.java
+++ 07_Collections/src/collections/ArrayList.java
@@ -1,7 +1,71 @@
 package collections;
 
-public class ArrayList<E> {
-	
-	private E[] array;
-	
+public class ArrayList<E> implements List<E> {
+
+	private Object[] array;
+
+	public boolean add(Object e) {
+		boolean added = false;
+		Object[] newArray;
+		if (array == null) {
+			newArray = new Object[1];
+			newArray[0] = e;
+		} else {
+			newArray = new Object[array.length + 1];
+			int i;
+			for (i = 0; i < array.length; i++) {
+				newArray[i] = array[i];
+			}
+			newArray[i] = e;
+		}
+		array = newArray;
+		added = true;
+		return added;
+	}
+
+	public boolean remove(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public boolean esEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean contains(Object o) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean add(int idex, Object e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public E remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public E get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object set(int index, Object e) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int index(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
--- 07_Collections/src/collections/List.java
+++ 07_Collections/src/collections/List.java
@@ -1,12 +1,14 @@
 package collections;
 
-public  interface List<E>{
-
+public interface List<E> extends Collection<E> {
 	public boolean add(int idex, E e);
+
 	public E remove(int index);
+
 	public E get(int index);
+
 	public E set(int index, E e);
+
 	public int index(Object o);
-	
-	
+
 }
